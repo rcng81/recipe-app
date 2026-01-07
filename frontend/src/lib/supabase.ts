7a0ca2acec0cec6755030599f5fcf622b18948fd
Russell Ng <98992360+rcng81@users.noreply.github.com>
2026-01-07 16:57:17 -0500

    Revert "Show like counts to guests by using public Supabase client"

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -10,14 +10,3 @@ export const supabase = createClient(
     },
   }
 );
-
-export const supabasePublic = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false,
-    },
-  }
-);
